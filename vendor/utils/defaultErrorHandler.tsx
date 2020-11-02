327580ebeafb04113152e1c7476bfe2a00d697ff
Piotr Bosak <piotr.hitori.bosak@gmail.com>
2020-11-02 13:32:44 +0100

    Handle _is404 in HandleErrors

--- vendor/utils/defaultErrorHandler.tsx
+++ vendor/utils/defaultErrorHandler.tsx
@@ -4,8 +4,8 @@ import Pages404 from '../../pages/404'
 export const HandleErrors = <T extends any>(
   Component: React.ComponentType<T>,
 ): React.ComponentType<T> => {
-  const HandleErrorsWrapper = (props: T & { _is403?: boolean }) => {
-    return props._is403 ? <Pages404 /> : <Component {...props} />
+  const HandleErrorsWrapper = (props: T & { _is403?: boolean; _is404?: boolean }) => {
+    return props._is403 || props._is404 ? <Pages404 /> : <Component {...props} />
   }
   return HandleErrorsWrapper
 }

d8d4770c78a014a2d66cccda02f4224c8e1974f4
Piotr Bosak <piotr.hitori.bosak@gmail.com>
2020-11-04 15:29:14 +0100

    Compact typecheck

--- vendor/config-type-check.ts
+++ vendor/config-type-check.ts
@@ -1,17 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars,no-unused-vars */
 import publicConfig from 'config/public.json'
 import privateConfig from 'config/private.json'
 
 /**
- * These function provide a way to check if app config contains required information.
+ * These assignments provide a way to check if app config contains required information.
  * Mods can extend PublicConfig and PrivateConfig interfaces to provide a way to configure them.
  *
  * Typescript will show errors here until you configure your app's config/public.json and
  * config/private.json to match the needs of PublicConfig and PrivateConfig interfaces
  */
-export function loadPublicConfig(): PublicConfig {
-  return publicConfig
-}
-
-export function loadPrivateConfig(): PrivateConfig {
-  return privateConfig
-}
+const privateConfigCheck = privateConfig as PrivateConfig
+const publicConfigCheck = publicConfig as PublicConfig

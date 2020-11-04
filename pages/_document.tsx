8a1f7c68bbcd4c7d9897b167148d705eb17be296
Piotr Bosak <piotr.hitori.bosak@gmail.com>
2020-11-04 16:04:58 +0100

    Remove polyfill from default codebase

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,9 +7,7 @@ export default class MyDocument extends VendorDocument {
   render(): JSX.Element {
     return (
       <Html>
-        <Head>
-          <script src='https://polyfill.io/v3/polyfill.min.js?features=ResizeObserver%2CIntersectionObserver%2Csmoothscroll%2CArray.isArray%2CArray.from%2CSet'></script>
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />

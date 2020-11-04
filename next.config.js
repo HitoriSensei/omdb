6b1b9a019aa33ff1573fff1ae18c9070700a3cd4
Piotr Bosak <piotr.hitori.bosak@gmail.com>
2020-11-04 18:23:00 +0100

    cleanup default next.config.js

--- next.config.js
+++ next.config.js
@@ -4,14 +4,8 @@ const merge = require('lodash/merge')
 const nextConfig = merge(
     vendorNextConfig,
     {
-        publicRuntimeConfig: {
-          GTM_ID: ''
-        },
         staticRewrites: [
-            {
-                source: '/polityka-cookies',
-                destination: '/api/sites/default/files/2020-06/polityka-prywatnosci-zywiec-zdroj-pl%20%281%29.pdf',
-            },
+            // Here put what you normally would put input rewrites() function result
         ]
     });
 

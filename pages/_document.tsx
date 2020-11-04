48ed843052142cb127c376c1322802f4fb7bb059
Piotr Bosak <piotr.hitori.bosak@gmail.com>
2020-11-04 16:04:39 +0100

    Remove GTM from default codebase

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Html, Main, NextScript } from 'next/document'
-import { GTMBody, GTMHead } from 'vendor/components/GTM'
 import { Head } from 'vendor/components/Required/Head'
 import { VendorDocument } from 'vendor/components/Required/VendorDocument/VendorDocument'
 
@@ -10,10 +9,8 @@ export default class MyDocument extends VendorDocument {
       <Html>
         <Head>
           <script src='https://polyfill.io/v3/polyfill.min.js?features=ResizeObserver%2CIntersectionObserver%2Csmoothscroll%2CArray.isArray%2CArray.from%2CSet'></script>
-          <GTMHead />
         </Head>
         <body>
-          <GTMBody />
           <Main />
           <NextScript />
         </body>
